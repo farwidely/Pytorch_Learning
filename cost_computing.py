6d10830d261c5108f3f7141ac96100a48009ac8d
Cyan <Cyan@git.com>
2023-09-06 20:47:24 +0800

    commit 13

---
+++ cost_computing.py
@@ -0,0 +1,17 @@
+import torch
+from thop import profile
+from torchsummary import summary
+
+
+model = torch.load("./xxx.pth")
+model.cpu()
+
+input = torch.randn(1, 1, 224, 224)
+input.to('cpu')
+
+flops, params = profile(model, inputs=(input, ))
+print('FLOPs = ' + str(flops/1000**3) + 'G')
+print('Params = ' + str(params/1000**2) + 'M')
+
+model.cuda()
+summary(model, input_size=(1, 224, 224))
--- test.py
+++ test.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 import torchvision
 
-
-model = torchvision.models.shufflenet_v2_x1_0()
-print(model)
+x = torch.tensor([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]], dtype=float)
+print(x)
+x.requires_grad_(True)
+y = x.view(1, 6, 2)
+print(type(x))
