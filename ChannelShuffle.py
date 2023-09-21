72e2a7e294dcbaba41378901212cdee24f81ee6e
widely <1761339903@qq.com>
2023-09-21 16:03:10 +0800

    commit Channel Shuffle

---
+++ ChannelShuffle.py
@@ -0,0 +1,23 @@
+import torch
+from torch import nn
+
+
+class ShuffleChannel(nn.Module):
+    def __init__(self, groups):
+        super(ShuffleChannel, self).__init__()
+        self.groups = groups
+
+    def forward(self, x):
+        batch_size, channels, height, width = x.size()
+        channels_per_group = channels // self.groups
+
+        # 将张量按通道数分组
+        x = x.view(batch_size, self.groups, channels_per_group, height, width)
+
+        # 交换组内通道顺序
+        x = torch.transpose(x, 1, 2).contiguous()
+
+        # 展开张量
+        x = x.view(batch_size, -1, height, width)
+
+        return x
--- test.py
+++ test.py
@@ -1,9 +1,6 @@
 import torch
-import torch.nn as nn
-import torchvision
 
-x = torch.tensor([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]], dtype=float)
-print(x)
-x.requires_grad_(True)
-y = x.view(1, 6, 2)
-print(type(x))
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
+
+print(torch.cuda.is_available())
